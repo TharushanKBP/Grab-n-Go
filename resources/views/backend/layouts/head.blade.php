76b2391ffffc5d92c39257d489d8aaa098b2fb25
TharushanKBP <129367666+TharushanKBP@users.noreply.github.com>
2023-08-09 15:03:07 +0530

    changed the logo

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -1,4 +1,4 @@
-<title>:: Lucid University :: Home</title>
+<title>:: GRAB-N-GO :: Home</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
