6bb7b337b0eccb803360ef2e3b457743aeffcb5b
TharushanKBP <129367666+TharushanKBP@users.noreply.github.com>
2023-08-31 16:45:03 +0530

    linked the banner/promotion images

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,11 @@ class IndexController extends Controller
 {
     public function home()
     {
+        $banner = Banner::where(['status' => 'active', 'condition' => 'banner'])->orderBy('id', 'DESC')->limit('5')->get();
+        $banners = Banner::where(['status' => 'active', 'condition' => 'promo'])->orderBy('id', 'DESC')->limit('5')->get();
         $product = Product::where(['status' => 'active'])->orderBy('id', 'DESC')->limit('15')->get();
-        return view('frontend.layouts.master', compact('product'));
+
+        return view('frontend.layouts.master', compact('banner', 'banners', 'product'));
     }
 
     public function userAuth()
