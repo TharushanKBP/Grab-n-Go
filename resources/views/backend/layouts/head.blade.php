5cfa5764d1edc879b121af69746022c5a5c7181d
TharushanKBP <129367666+TharushanKBP@users.noreply.github.com>
2023-09-06 21:10:35 +0530

    set the favicon icon

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -1,4 +1,5 @@
-<title>:: GRAB-N-GO :: Home</title>
+<title>:: GRAB-N-GO :: Admin</title>
+    <link rel="shortcut icon" href="{{asset('frontend/assests/images/GNG-FAVICON.png')}}" type="image/x-icon">
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -2,6 +2,9 @@
 
 <head>
 
+    <title>:: GRAB-N-GO :: Home</title>
+    <link rel="shortcut icon" href="{{asset('frontend/assests/images/GNG-FAVICON.png')}}" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
     <link href="{{asset('frontend/assests/css/header-style.css')}}" rel="stylesheet">
